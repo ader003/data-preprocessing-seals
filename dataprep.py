c0ce98f435e88ff245dc0eaa9b8ae435653e5ce4
Audrey Der <ader003@ucr.edu>
2018-07-09 14:24:31 -0700

    more skeleton

--- dataprep.py
+++ dataprep.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pandas as pd
+import scipy as sp # for saving as .mat
+import os # for getting filepath
 
-data = pd.read_csv('/raw_data/groucho/03-09-15.csv', sep=",", names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
+# find file and open
 
+# get list of filenames 
 
+# for filename in filenames: (do the following)
+data = pd.read_csv(os.path.abspath(filename), sep=",", names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
 
+# drop columns
+data.drop(['behaviour_event', 'type_event', 'fps', 'time', 'location', 'raw', 'date', 'doe'], axis=1)
 
+# save as .mat
+# sp.io.savemat(filename, appendmat=True)
 
 
 def main():
