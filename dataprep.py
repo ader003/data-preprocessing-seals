d654320c530bab82c2642d0b03ab65d2a5e3cb9e
Audrey Der <ader003@ucr.edu>
2018-07-09 14:00:35 -0700

    added script base

---
+++ dataprep.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('/raw_data/groucho/03-09-15.csv', sep=",", names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
+
+
+
+
+
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
