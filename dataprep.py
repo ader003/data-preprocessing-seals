6dae713b36156e1df593d755d0082c820e3ec4da
Audrey Der <ader003@ucr.edu>
2018-07-10 12:25:31 -0700

    dropped columns

--- dataprep.py
+++ dataprep.py
@@ -1,25 +1,29 @@
 import numpy as np
 import pandas as pd
-import scipy as sp # for saving as .mat
+import scipy.io as sio # for saving as .mat
 import os # for getting filepath
-
-# find file and open
-
-# get list of filenames 
-
-# for filename in filenames: (do the following)
-data = pd.read_csv(os.path.abspath(filename), sep=",", names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
-
-# drop columns
-data.drop(['behaviour_event', 'type_event', 'fps', 'time', 'location', 'raw', 'date', 'doe'], axis=1)
-
-# save as .mat
-# sp.io.savemat(filename, appendmat=True)
-
+import copy
 
 def main():
+    dir_name = input("Directory name: ")
+    # get list of files
+    contents = os.listdir(dir_name)
+    filename = copy.deepcopy(contents)
+ 
+    # for each file, preprocess data and save as .mat
+    for i in range(len(contents)):
+        contents[i] = os.path.join(os.path.relpath(dir_name), contents[i])
+        data = pd.read_csv(contents[i], sep=",", names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
 
-    return
+        # drop columns
+        drop_cols = ['behaviour_event', 'type_event', 'fps', 'time', 'location', 'raw', 'date', 'doe']
+        data.drop(drop_cols, axis=1, inplace=True)
+        print(data.head())
+        # save as .mat
+        # dict_data = {'number': data['number'], 'value': data['value'], 'depth': data['depth'], 'x': data['x'], 'y': data['y'], 'z': data['z'], 'behaviour': data['behaviour'], 'type': data['type'], 'place': data['place'], } # dictionary to save MATLAB data in/to
+        # sio.savemat(filename[i], dict_data, appendmat=True)
+        data.to_csv(filename[i], sep=",")
+
 
 if __name__ == "__main__":
     main()
