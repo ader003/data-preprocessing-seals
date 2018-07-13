dfdd059c79dcbedb0e404d27670168db50dd1f0c
Audrey Der <ader003@ucr.edu>
2018-07-12 22:24:49 -0700

    more

--- dataprep.py
+++ dataprep.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import scipy.io as sio # for saving as .mat
 import os # for getting filepath
@@ -13,24 +12,19 @@ def main():
     # for each file, preprocess data and save as .csv
     for i in range(len(contents)):
         contents[i] = os.path.join(os.path.relpath(dir_name), contents[i])
-        data = pd.read_csv(contents[i], sep=",", header=0, names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'])
+        data = pd.read_csv(contents[i], sep=",", header=0, names=['number', 'behaviour_event', 'value', 'type_event', 'fps', 'time', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'location', 'place', 'raw', 'date', 'doe'], low_memory=False)
 
         # drop columns
         drop_cols = ['behaviour_event', 'type_event', 'fps', 'time', 'location', 'raw', 'date', 'doe']
         data.drop(drop_cols, axis=1, inplace=True)
-        
+            
         # replace values in 'behaviour', 'type', and 'place'
-        data['behaviour'].replace(to_replace={'other': 0, 'moving': 1, 'in': 2, 'foraging': 3, 'manipulation': 4, 'chewing': 5, 'thrash': 6, 'slow': 7, 'swimming': 8, 'holdntear': 9, 'still': 10, 'out': 11, 'sitting': 12}, inplace=True)
-        data['type'].replace(to_replace={'Other': 0, 'Travelling': 1, 'Foraging': 2, 'Resting': 3}, inplace=True)
+        data['behaviour'].replace(to_replace={'other': 0, 'moving': 1, 'in': 2, 'foraging': 3, 'manipulation': 4, 'chewing': 5, 'thrash': 6, 'slow': 7, 'swimming': 8, 'holdntear': 9, 'still': 10, 'out': 11, 'sitting': 12, 'sailing': 13, 'rubbing': 14, 'feeding': 15, 'scratch': 16, 'lying': 17, 'jump': 18, 'fast': 19, 'facerub': 20, 'bowing': 21, 'jugging': 22, 'playing': 23, 'shake': 24, 'grooming': 25, 'stationary': 26}, inplace=True)
+        data['type'].replace(to_replace={'Other': 0, 'Travelling': 1, 'Foraging': 2, 'Resting': 3, 'Grooming': 4}, inplace=True)
         data['place'].replace(to_replace={'land': 1, 'surface': 0, 'underwater': -1}, inplace=True)
-        # print(data.head())
         print("Processed " + str(filename[i]))
 
-        # save as .mat
-        # dict_data = {'number': data['number'], 'value': data['value'], 'depth': data['depth'], 'x': data['x'], 'y': data['y'], 'z': data['z'], 'behaviour': data['behaviour'], 'type': data['type'], 'place': data['place'], } # dictionary to save MATLAB data in/to
-        # sio.savemat(filename[i], dict_data, appendmat=True)
-        
-        # UNCOMMENT AFTER TESTING:
+        # save as .csv -- will save in one directory above
         data.to_csv(filename[i], sep=",", header=['number', 'value', 'depth', 'x', 'y', 'z', 'behaviour', 'type', 'place'])
 
 
---
+++ test.m
@@ -0,0 +1,20 @@
+% Nov 16
+% Converting CSV files to Matlab variable files in Batch
+
+
+clc;
+close all;
+clear all;
+
+matfiles = dir('*.csv');
+for ind = 1:length(matfiles)
+    disp 'Processing:';
+    a = mat2str(matfiles(ind).name);
+    disp a;
+    a = strrep(a,'.csv','');
+    importdata(matfiles(ind).name);
+%     eval(sprintf('%s = M;',a));
+    eval(sprintf('save %s;',a));
+    clear a M 
+end
+disp 'Done ...';
